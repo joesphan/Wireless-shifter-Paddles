587dcf02cdca2090c8cc3db7e38bf39f45a4feb4
joesphan <joesphanlu@gmail.com>
2020-09-29 18:21:25 -0700

    Firmware done, README done

--- PaddlesFirmware/PaddlesFirmware.ino
+++ PaddlesFirmware/PaddlesFirmware.ino
@@ -12,7 +12,9 @@
 #define RIGHT_PADDLE_PIN 11
 #define LEFT_PADDLE_PIN 7
 #define HORN_PIN 9
+#define ACTIVE_PIN 10
 
+#define TIMEOUT 600000    //10 minutes
 WiFiUDP Udp;
 
 IPAddress ip(192, 168, 4, 1);
@@ -20,46 +22,45 @@ IPAddress ip(192, 168, 4, 1);
 const char* ssid     = STASSID;
 const char* password = STAPSK;
 
-byte packet[9] = {0,0,0,0,0,0,0,0,0};
+byte packet[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+int stopTime;
 
 void setup() {
-  Serial.begin(115200);
-
-  // We start by connecting to a WiFi network
-
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
-     would try to act as both a client and an access-point and could cause
-     network-issues with your other WiFi-devices on your WiFi-network. */
+  stopTime = millis() + TIMEOUT;   //reset timer;
+  pinMode(ACTIVE_PIN, OUTPUT);    //disable reset
+  digitalWrite(ACTIVE_PIN, HIGH);
+  
+  pinMode(LEFT_PADDLE_PIN, INPUT_PULLUP);
+  pinMode(RIGHT_PADDLE_PIN, INPUT_PULLUP);
+  pinMode(HORN_PIN, INPUT_PULLUP);
+  
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+    if (millis() == stopTime) {   //sleep if not connected for a long time
+      ESP.deepSleep(0);
+    }
   }
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
   packet[0] = 'B';
-  pinMode(LEFT_PADDLE_PIN, INPUT_PULLUP);
-  pinMode(RIGHT_PADDLE_PIN, INPUT_PULLUP);
-  pinMode(HORN_PIN, INPUT_PULLUP);
 }
 
 void loop() {
   readStates();
   sendPacket();
+  if (packet[0] || packet[1] || packet[2]) {
+    stopTime = millis() + TIMEOUT;   //reset timer;
+  }
+  if (millis() == stopTime) {   //sleep if not connected for a long time
+    ESP.deepSleep(0);
+  }
 }
 void readStates() {
+  packet[0] = !digitalRead(RIGHT_PADDLE_PIN);
   packet[1] = !digitalRead(LEFT_PADDLE_PIN);
-  packet[2] = !digitalRead(RIGHT_PADDLE_PIN);
+  packet[2] = !digitalRead(HORN_PIN);
 }
 
 void sendPacket() {
@@ -68,12 +69,12 @@ void sendPacket() {
   Udp.write(packet[1]);
   Udp.write(packet[2]);
   /*
-  Udp.write(packet[3]);
-  Udp.write(packet[4]);
-  Udp.write(packet[5]);
-  Udp.write(packet[6]);
-  Udp.write(packet[7]);
-  Udp.write(packet[8]);
+    Udp.write(packet[3]);
+    Udp.write(packet[4]);
+    Udp.write(packet[5]);
+    Udp.write(packet[6]);
+    Udp.write(packet[7]);
+    Udp.write(packet[8]);
   */
   Udp.endPacket();
 }
--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 wireless shifter pedals. compatible with unified Sim controller.
 
 ## Firmware
-Sleep mode occurs after 10 minutes. Pressing any button resets this timer and 
+Sleep mode occurs after 10 minutes. Pressing any button resets this timer.
+Right paddle wakes the device. Because the ESP8266 can only be woken up by reset, an active pin will be needed to signify the device is awake, external circuitry handles the disabling of the reset.
 The packet is documented in \PacketInfo.docx
 ## Power Supply
 
